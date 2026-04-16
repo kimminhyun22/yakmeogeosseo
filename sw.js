515d17ad11d4c7cdc4860e30b0ab8e48f55c27d6
kimminhyun22 <101535722+kimminhyun22@users.noreply.github.com>
2026-04-16 11:50:14 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,25 +1,14 @@
-const CACHE = 'yak-v2';
-const ASSETS = [
-  '/yakmeogeosseo/',
-  '/yakmeogeosseo/index.html',
-  '/yakmeogeosseo/manifest.json',
-  '/yakmeogeosseo/icon-192.png',
-  '/yakmeogeosseo/icon-512.png'
-];
-
 self.addEventListener('install', e => {
   self.skipWaiting();
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
 });
 
 self.addEventListener('activate', e => {
   e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-    )
+    caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
   );
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
 });
